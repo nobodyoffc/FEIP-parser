66ac6c3df8d50ded1269938547135466edd5a6b0
nobodyoffc <nobodyoffc@gmail.com>
2023-01-19 15:11:31 +0800

    delete unused class

--- src/main/java/tools/ParseTools.java
+++ src/main/java/tools/ParseTools.java
@@ -1,13 +1,6 @@
 package tools;
 
-import java.io.IOException;
-
 import com.google.gson.Gson;
-
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch._types.ElasticsearchException;
-import co.elastic.clients.elasticsearch._types.SortOrder;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
 
 public class ParseTools {
 		
@@ -15,14 +8,5 @@ public class ParseTools {
 		Gson gson = new Gson();
 		System.out.println("***********\n"+ob.getClass().toString()+": "+gson.toJson(ob)+"\n***********");
 		return ;
-	}
-
-	public static Block getBestBlock(ElasticsearchClient esClient) throws ElasticsearchException, IOException {
-		SearchResponse<Block> result = esClient.search(s->s
-				.index("block")
-				.size(1)
-				.sort(so->so.field(f->f.field("height").order(SortOrder.Desc)))
-				, Block.class);
-		return result.hits().hits().get(0).source();
 	}
 }
